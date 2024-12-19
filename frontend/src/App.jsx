517a20a457a313357afead0551b8c4926f5af1bf
Ajibade Adam <dapoajibade66@gmail.com>
2024-12-19 08:18:44 +0100

    Refactor App component to utilize Suspense for loading states and improve user experience during dynamic imports of pages and components.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Navigate, Route, Routes } from "react-router";
 const SignupPage = React.lazy(() => import("./pages/SignupPage"));
 const LoginPage = React.lazy(() => import("./pages/LoginPage"));
@@ -27,124 +27,127 @@ const ResetPassword = React.lazy(() =>
   import("./components/ResetPasswordComps/ResetPassword")
 );
 import { Link } from "react-router-dom";
+import PageIsLoading from "./components/ui/PageIsLoading";
 
 function App() {
   return (
-    <Routes>
-      <Route element={<ProtectedRoutes />}>
-        {/* Explore route */}
-        <Route
-          path="/"
-          element={
-            <Layout>
-              <ExplorePage />
-            </Layout>
-          }
-        />
-        {/* Search route */}
-        <Route
-          path="/search"
-          element={
-            <Layout>
-              <SearchPage />
-            </Layout>
-          }
-        />
-        {/* Profile settings route */}
-        <Route
-          path="/settings"
-          element={
-            <Layout>
-              <SettingsPage />
-            </Layout>
-          }
-        />
-        {/* Library route where crafted and favorite quizzes
+    <Suspense fallback={<PageIsLoading />}>
+      <Routes>
+        <Route element={<ProtectedRoutes />}>
+          {/* Explore route */}
+          <Route
+            path="/"
+            element={
+              <Layout>
+                <ExplorePage />
+              </Layout>
+            }
+          />
+          {/* Search route */}
+          <Route
+            path="/search"
+            element={
+              <Layout>
+                <SearchPage />
+              </Layout>
+            }
+          />
+          {/* Profile settings route */}
+          <Route
+            path="/settings"
+            element={
+              <Layout>
+                <SettingsPage />
+              </Layout>
+            }
+          />
+          {/* Library route where crafted and favorite quizzes
         will all be accessed */}
-        <Route
-          path="/library"
-          element={
-            <Layout text={"Your library"}>
-              <LibraryPage />
-            </Layout>
-          }
-        />
-        {/* This is where quiz will be crafted */}
-        <Route path="/quiz-editor/:id" element={<QuizEditorPage />} />
-        {/* Solo quiz route */}
-        <Route path="/play/:id" element={<PlayQuizPage />} />
-        {/* live quiz route */}
-        <Route path="/play-live/:id" element={<PlayLiveQuizPage />} />
-        {/* Quiz details route */}
-        <Route
-          path="/details/:id"
-          element={
-            <Layout>
-              <DetailsPage />
-            </Layout>
-          }
-        />
-        {/* Join live route */}
-        <Route path="/join-live" element={<JoinLivePage />}>
-          {/* Where code will be inputed */}
-          <Route index element={<Join />} />
-          {/* where quiz live profile will be setup, and where quiz Participants
+          <Route
+            path="/library"
+            element={
+              <Layout text={"Your library"}>
+                <LibraryPage />
+              </Layout>
+            }
+          />
+          {/* This is where quiz will be crafted */}
+          <Route path="/quiz-editor/:id" element={<QuizEditorPage />} />
+          {/* Solo quiz route */}
+          <Route path="/play/:id" element={<PlayQuizPage />} />
+          {/* live quiz route */}
+          <Route path="/play-live/:id" element={<PlayLiveQuizPage />} />
+          {/* Quiz details route */}
+          <Route
+            path="/details/:id"
+            element={
+              <Layout>
+                <DetailsPage />
+              </Layout>
+            }
+          />
+          {/* Join live route */}
+          <Route path="/join-live" element={<JoinLivePage />}>
+            {/* Where code will be inputed */}
+            <Route index element={<Join />} />
+            {/* where quiz live profile will be setup, and where quiz Participants
            will be displayed */}
-          <Route path=":id" element={<Room />} />
+            <Route path=":id" element={<Room />} />
+          </Route>
+          {/* Hosted live quiz route */}
+          <Route path="/host-live/:id" element={<HostLivePage />} />
+          {/* Reports route */}
+          <Route
+            path="/reports"
+            element={
+              <Layout text="Reports">
+                <ReportsPage />
+              </Layout>
+            }
+          />
+          {/* Host report route */}
+          <Route
+            path="/host-report/:id"
+            element={
+              <Layout>
+                <HostReportPage />
+              </Layout>
+            }
+          />
+          {/* Quiz result route */}
+          <Route
+            path="/result/:id"
+            element={
+              <Layout>
+                <ResultPage />
+              </Layout>
+            }
+          />
         </Route>
-        {/* Hosted live quiz route */}
-        <Route path="/host-live/:id" element={<HostLivePage />} />
-        {/* Reports route */}
-        <Route
-          path="/reports"
-          element={
-            <Layout text="Reports">
-              <ReportsPage />
-            </Layout>
-          }
-        />
-        {/* Host report route */}
-        <Route
-          path="/host-report/:id"
-          element={
-            <Layout>
-              <HostReportPage />
-            </Layout>
-          }
-        />
-        {/* Quiz result route */}
-        <Route
-          path="/result/:id"
-          element={
-            <Layout>
-              <ResultPage />
-            </Layout>
-          }
-        />
-      </Route>
 
-      {/* Non protected routes */}
-      <Route
-        path="/404"
-        element={
-          <div className="flex flex-col gap-4 justify-center items-center">
-            <h1 className="text-4xl p-5 isidoraBold">
-              404 not found😥
-              <Link to="/" className="py-3 px-6 bg-shinyPurple">
-                Go Back
-              </Link>
-            </h1>
-          </div>
-        }
-      />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/reset-password" element={<ResetPasswordPage />}>
-        <Route index element={<ForgotPassword />} />
-        <Route path=":token" element={<ResetPassword />} />
-      </Route>
-      <Route path="*" element={<Navigate to="/404" />} />
-    </Routes>
+        {/* Non protected routes */}
+        <Route
+          path="/404"
+          element={
+            <div className="flex flex-col gap-4 justify-center items-center">
+              <h1 className="text-4xl p-5 isidoraBold">
+                404 not found😥
+                <Link to="/" className="py-3 px-6 bg-shinyPurple">
+                  Go Back
+                </Link>
+              </h1>
+            </div>
+          }
+        />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />}>
+          <Route index element={<ForgotPassword />} />
+          <Route path=":token" element={<ResetPassword />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/404" />} />
+      </Routes>
+    </Suspense>
   );
 }
 
